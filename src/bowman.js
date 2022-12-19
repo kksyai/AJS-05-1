7a6cfc26ef5be4463f99664eef4dee95267ebb01
YanaRu <62380481+kksyai@users.noreply.github.com>
2022-12-19 23:28:08 +0300

    Update bowman.js

--- src/bowman.js
+++ src/bowman.js
@@ -2,7 +2,7 @@ import Character from './character';
 
 class Bowman extends Character {
   constructor(name, type = 'Bowman') {
-    super(name);
+    super(name, type);
     this.attack = 25;
     this.defence = 25;
   }
