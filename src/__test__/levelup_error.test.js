92b0b9f7a722b3bf722c2f0c97598193c9a9fd8e
YanaRu <kksyai@gmail.com>
2023-01-18 17:56:03 +0300

    levelUp test correct

--- src/__test__/levelup_error.test.js
+++ src/__test__/levelup_error.test.js
@@ -1,7 +1,9 @@
 import Bowman from '../bowman';
 
 test('throw levelUp error', () => {
-  const person = new Bowman('oleg');
-  person.health = 0;
-  expect(person.levelUp()).toThrow(Error('нельзя повысить уровень жизни умершего'));
+  expect(() => {
+    const person = new Bowman('oleg');
+    person.health = 0;
+    person.levelUp();
+  }).toThrow(Error('нельзя повысить уровень жизни умершего'));
 });
